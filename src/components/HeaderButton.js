fb2a604f929e4fda2dba4b1868d2de1331f3d5db
Josh Freeman <jdfreeman11@gmail.com>
2018-06-22 16:17:54 -0500

    Activate button upon link

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const HeaderButton = ({linkTo, text, ...props}) => (
-  <Link to={linkTo} className="walker-button" {...props} >
+  <Link to={linkTo} className="walker-button" {...props} activeClassName='active-link' >
     <div className="nav-style top"></div>
     <div className="nav-style left"></div>
     {text}
