94737691707bb83b7e94bc2e8264d70d69543242
Josh Freeman <jdfreeman11@gmail.com>
2018-06-27 07:52:53 -0500

    Enable Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-121406556-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    }
   ],
 }
